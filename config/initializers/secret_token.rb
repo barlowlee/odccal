428f82befcd8b32359250b0bc0e80229e8582492
Jonathan Lee <jlee@ctgla.org>
2012-11-04 21:50:40 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odccal::Application.config.secret_token = 'fade7c9522fcea265c1b1524d2c78c94675850b375c04548fa40d624955573a3b80ccad24d220f240b364a1427cbac05f4587d1b8893bb3e6f8a961c9f3c3c80'
